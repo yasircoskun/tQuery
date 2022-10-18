07fea43d5637052e8c1566d78ff05e7a3d098fcb
yasircoskun <yasir.coskun@outlook.com>
2022-10-18 06:56:14 +0300

    ₺Query is a simple js preprocessor

---
+++ ₺.js
@@ -0,0 +1,93 @@
+class PreProcessor {
+    constructor(type, transformers) {
+      this.type = type;
+      this.transformers = transformers
+    }
+
+    getSrc(script_element) {
+      let src = script_element.getAttribute('src');
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', src, false);
+      xhr.send();
+      return xhr.responseText;
+    }
+
+    getInnerHTML(script_element) {
+      return script_element.innerHTML;
+    }
+
+    getCode(script_element) {
+      if(script_element.src){
+        return this.getSrc(script_element);
+      }else{
+        return this.getInnerHTML(script_element);
+      }
+    }
+
+    Async = class {
+      getInnerHTML(script_element) {
+        return new Promise((resolve, reject) => {
+          resolve(this.getInnerHTML());
+        });
+      }
+
+      getSrc(script_element) {
+        let src = script_element.getAttribute('src');
+        return fetch(src).then(response => response.text());
+      }
+
+      getCode(script_element) {
+        if(script_element.src){
+          return this.getSrcAsync(script_element);
+        }else{
+          return this.getInnerHTMLAsync(script_element);
+        }
+      }
+    }
+
+    transform(callback) {
+      let scripts = document.querySelectorAll(`script[type="${this.type}"]`);
+      scripts.forEach((script) => {
+        let code = this.getCode(script);
+        this.transformers.forEach((transformer) => {
+          code = transformer.transform(code);
+        });
+        console.log(code)
+        if(callback){
+          callback(code);
+        } else {
+          this.evaluate(code);
+        }
+        script.innerHTML = code;
+      });
+    }
+
+    evaluate(code) {
+      eval(code);
+    }
+}
+
+class Transformer {
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+  }
+
+  transform(code) {
+    if(this.from instanceof RegExp){
+      return code.replace(this.from, this.to);
+    } else {
+      return code.split(this.from).join(this.to);
+    }
+  }
+}
+
+function tQuery(){
+  new PreProcessor('text/₺Query', [
+    new Transformer(/(₺*?)[\.\(]+/g, (match, p1) => {
+      return match.replace('₺', '$');
+    }),
+  ]).transform();
+}
+
+tQuery();
